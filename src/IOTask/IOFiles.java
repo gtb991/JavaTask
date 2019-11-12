e041732fbdcc9b94b75494ae315385c3258905f3
gburlac <56862163+gburlac@users.noreply.github.com>
2019-11-12 17:01:46 +0200

    Start IO.

--- src/IOTask/IOFiles.java
+++ src/IOTask/IOFiles.java
@@ -1,12 +1,16 @@
 package IOTask;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class IOFiles {
 
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) throws Exception {
 
         FileReader in = null;
         FileWriter out = null;
@@ -17,7 +21,7 @@ public class IOFiles {
 
             int c;
             while ((c = in.read()) != -1) {
-                System.out.println("value : " + (char) c);
+//                System.out.println("value : " + (char) c);
                 out.write(c);
             }
         } finally {
@@ -25,5 +29,30 @@ public class IOFiles {
                 in.close();
             }
         }
+
+        Path path = Paths.get("C:\\javaxAutomation\\inPut.txt");
+        List<String> contents = Files.readAllLines(path);
+        for (String content : contents) {
+//            System.out.println(content);
+        }
+
+        FileReader fileReader = new FileReader("C:\\javaxAutomation\\inPut.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        int i;
+        while ((i = bufferedReader.read()) != -1) ;
+        System.out.println((char) i);
+         bufferedReader.close();
+         fileReader.close();
+
+
+        FileInputStream inputStream = new FileInputStream("C:\\javaxAutomation\\inPut.txt");
+        ReadableByteChannel source = inputStream.getChannel();
+        FileOutputStream outputStream = new FileOutputStream("C:\\javaxAutomation\\outPut.txt");
+        WritableByteChannel destination = outputStream.getChannel();
+//        copyData(source,destination);
+        source.close();
+        destination.close();
+
+    
     }
 }
